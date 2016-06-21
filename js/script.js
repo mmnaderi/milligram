55d6103c4d4a033dc1529ad4b5154aaf3c2f479b
Mohammad Mahdi Naderi <mmnaderi.ir@gmail.com>
2016-06-21 04:45:00 +0430

    Make RTL and translate to persian

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@
 		document.addEventListener('click', closePopover);
 		buildSnippets();
 		getVersion();
-		googleAnalytics();
 		shareDialog();
 		localhost();
 	}
@@ -51,29 +50,18 @@
 
 	function getVersion() {
 		if ($description || $download) {
-			request.open('GET', '//raw.githubusercontent.com/milligram/milligram/master/package.json', true);
+			request.open('GET', '//raw.githubusercontent.com/mmnaderi/milligram-rtl/master/package.json', true);
 			request.onload = function() {
 				if (this.status >= 200 && this.status < 400) {
 					var version = JSON.parse( this.response ).version;
-					if ($description) $description.innerHTML = $description.innerHTML+' <br><i><small>Currently v'+version+'</small></i>';
-					$download.setAttribute('href', 'https://github.com/milligram/milligram/archive/v'+version+'.zip');
+					if ($description) $description.innerHTML = $description.innerHTML+' <br><i><small>ورژن فعلی v'+version+'</small></i>';
+					$download.setAttribute('href', 'https://github.com/mmnaderi/milligram-rtl/archive/v'+version+'.zip');
 				}
 				else {
 					console.log( '// There was a connection error of some sort' );
 				}
 			};
 			request.send();
-		}
-	}
-
-	function googleAnalytics() {
-		if ( window.location.hostname !== 'localhost' ) {
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-24389952-15', 'auto');
-			ga('send', 'pageview');
 		}
 	}
 
